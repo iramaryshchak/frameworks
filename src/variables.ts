5cee56bc3aaae65ca1bea1a1c4169a1b608b57fb
IraMarys <maryshchak.iryna@chnu.edu.ua>
2024-08-20 15:59:12 +0300

    lab1

---
+++ src/variables.ts
@@ -0,0 +1,19 @@
+// Оголошення змінних різних типів
+let typeString: string = "Good day!";
+let typeNumber: number = 42;
+let typeBoolean: boolean = true;
+let typeAny: any = "any type";
+
+// Виведення значень у консоль
+console.log("String:", typeString);
+console.log("Number:", typeNumber);
+console.log("Boolean:", typeBoolean);
+console.log("Any:", typeAny);
+
+// Оголошення масивів
+let stringArray: string[] = ["One", "Two", "Three"];
+let numberArray: number[] = [1, 2, 3, 4, 5];
+
+// Виведення масивів у консоль
+console.log("String Array:", stringArray);
+console.log("Number Array:", numberArray);
